4072d4ba0517fa4097726b9135efa017b0919c82
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-04-30 14:43:40 -0500

    Enviando archivos de compilacion

---
+++ src/main/java/co/edu/usco/pw/spring/swagger/SpringBootSwagger3ExampleApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usco.pw.spring.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwagger3ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger3ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usco/pw/spring/swagger/SpringBootSwagger3ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco.pw.spring.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger3ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
